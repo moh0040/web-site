d112c6aa30a177b03d9c1993a9860dbb8b0835e1
saeed <sharifi.vsb@gmail.com>
2018-05-27 01:11:54 +0200

    add angular material and fxFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {NgModule} from '@angular/core';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {FormsModule} from "@angular/forms";
+import {MatCheckboxModule} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatCheckboxModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
